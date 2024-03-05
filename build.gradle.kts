0fdd5114344edaeb46b19da73543a4e28af8024f
Clayder <peterclayder@gmail.com>
2024-03-04 22:46:06 -0300

    feat: added libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("com.github.jmongard.git-semver-plugin") version "0.12.4"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
 group = "br.clayder.finansys.invoice"
